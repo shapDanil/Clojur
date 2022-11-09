a39669d73335430b6119d0ef3c2334f129631252
shapDanil <114206592+shapDanil@users.noreply.github.com>
2022-11-09 08:23:20 +0700

    all fixed

--- lab1Clojur/src/untitled/Lab12.clj
+++ lab1Clojur/src/untitled/Lab12.clj
@@ -1,15 +1,20 @@
 (ns untitled.Lab12)
+
+
 (defn add-head [set symbols]
   (loop [set-loop set symbols-loop symbols sets-loop (list)]
     (if (empty? symbols-loop)
       sets-loop
       (if (not= (first set) (first symbols-loop))
-        (recur set-loop (rest symbols-loop) (cons (cons (first symbols-loop) set-loop) sets-loop) )
-        (recur set-loop (rest symbols-loop) sets-loop)
-        )
-      )
-    )
-  )
+        (recur set-loop (rest symbols-loop) (cons (cons (first symbols-loop) set-loop) sets-loop))
+        (recur set-loop (rest symbols-loop) sets-loop)))))
+(defn add-head [set symbols]
+  (loop [set-loop set symbols-loop symbols sets-loop (list)]
+    (if (empty? symbols-loop)
+      sets-loop
+      (recur set-loop (rest symbols-loop) (if (not= (first set) (first symbols-loop))
+                                            (cons (cons (first symbols-loop) set-loop) sets-loop)
+                                            sets-loop)))))
 (defn gen-step-set [sets symbols]
   (loop [sets-loop sets  new-sets-loop (list)]
     (if (empty? sets-loop)
@@ -17,14 +22,16 @@
       (recur (rest sets-loop) (concat new-sets-loop (add-head (first sets-loop) symbols))))))
 
 (defn step [sets symbols n]
-  (if (not= n 0)
-    (gen-step-set sets symbols)
+  (if (= n 0)
+    sets
     (recur (gen-step-set sets symbols) symbols (dec n))))
 
 
 (defn solve [symbols n]
-  (if (not= n 0)
+  (if (>= n 0)
     (step (list(list)) symbols n)
     ()))
 
-(println (solve (list "a" "b" "c") 5))
+
+
+(println (solve (list "a" "b" "c") 3))
--- lab1Clojur/src/untitled/Lab13.clj
+++ lab1Clojur/src/untitled/Lab13.clj
@@ -1,14 +1,27 @@
 (ns untitled.Lab13)
 
-(defn my-reduce [f acc coll]                                ; слышал,что вы говорили кому-то это сделать
+
+; если использовать так то при больших последовательностей будет переполнение стека
+(defn my-reduce [f acc coll]           ; слышал,что вы говорили кому-то это сделать
   (if (empty? (rest coll))
     (f acc (first coll))
-    (f acc (my-reduce f (first coll) (rest coll)))))
+    (my-reduce f (f acc (first coll)) (rest coll))))
+; так должна решаться проблема выше
+(defn my-reduce [func acc coll]
+  (if (empty? coll)
+    acc
+    (recur func (func acc (first coll)) (rest coll))))
 
-(defn my-map [f coll] 
+
+(defn my-map [f coll]   ;// без реверса
+  (letfn [(func [acc x] (concat acc (list (f x))))]
+    (my-reduce func nil coll)))
+(defn my-map [f coll]
   (letfn [(func [acc x] (conj acc (f x)))]
     (reverse (my-reduce func nil coll))))
-
+(defn my-filter [f coll]  ;// без реверса
+  (letfn [(func [acc x] (concat acc (if (f x) (list x))))]
+    (my-reduce func nil coll)))
 (defn my-filter [f coll]
   (letfn [(func [acc x] (if (f x) (conj acc x) acc))]
     (reverse (my-reduce func nil coll))))
@@ -16,12 +29,12 @@
 (filter even? (range 15))
 (my-filter even? (range 15))
 ;///////////////////////////////////////
-(my-map dec '(3 4 6 75))
-(map dec '(3 4 6 75))
-(my-reduce + 5 '(1 2 3 4))
-(reduce + 5 '(1 2 3 4))
-(my-filter even? (range 7))
-(filter even? (range 7))
-(my-filter pos? (list 2 1 0 -4 -5 4 -4 2))
-(filter pos? (list 2 1 0 -4 -5 4 -4 2))
+(println(my-map dec '(3 4 6 75)))
+(println(map dec '(3 4 6 75)))
+(println(my-reduce + 5 '(1 2 3 4)))
+(println(reduce + 5 '(1 2 3 4)))
+(println(my-filter even? (range 7)))
+(println(filter even? (range 7)))
+(println(my-filter pos? (list 2 1 0 -4 -5 4 -4 2)))
+(println(filter pos? (list 2 1 0 -4 -5 4 -4 2)))
 
